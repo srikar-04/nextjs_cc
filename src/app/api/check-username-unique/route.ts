24c7c2d01dab2bd5663a2a7aec4e0f7a3343d981
srikar <srikar.code01@gmail.com>
2025-03-02 16:54:32 +0530

    add API endpoint to check username uniqueness with validation and error handling

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,57 @@
+import { z } from "zod";
+import UserModel from "@/models/User.models";
+import dbConnect from "@/lib/dbConnect";
+import { usernameValidation } from "@/schemas/signUpSchema";
+import { log } from "console";
+
+const UsernameQuerySchema = z.object({
+    username: usernameValidation
+})
+
+export async function GET(request: Request)  {  // this "Request" is predefinied Interface
+    await dbConnect();
+
+    try {
+
+        const { searchParams }  = new URL(request.url)
+        const queryParam = {
+            username: searchParams.get('username')
+        }
+        // validation with zod
+        const result = UsernameQuerySchema.safeParse(queryParam)
+        if(!result?.success) {
+            const usernamError = result.error.format().username?._errors || []  // .format is used to just structurize errors into array of objects
+            return Response.json({
+                success: false,
+                message: 'Invalid username',
+                errors: usernamError.length > 0 ? usernamError.join(', ') : 'Invalid query parameter'
+            }, { status: 400 })
+        }
+
+        const { username } = result.data // this data contains the username ->> check zod docs while using safeParse
+        console.log(username, 'username');
+
+        const exsistingVerifiedUser = await UserModel.findOne({username, isVerified: true})
+        console.log(exsistingVerifiedUser, 'exsisting user from database');
+        
+
+        if(exsistingVerifiedUser) {
+            return Response.json({
+                success: false,
+                message: 'Username is already taken'
+            }, {status: 400})
+        }
+
+        return Response.json({
+            success: true,
+            message: 'Username is unique'
+        })
+        
+    } catch (error) {
+        console.error('error checking unique usernam: ', error)
+        return Response.json({   // this response is also an interface
+            success: false,
+            message: 'Error checking unique username'
+        }, {status: 500})
+    }
+}
