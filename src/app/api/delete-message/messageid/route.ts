27bc22218e1e8e95dc10ae663212035dc7ae37d9
srikar <srikar.code01@gmail.com>
2025-03-07 16:10:59 +0530

    implement message deletion logic and enhance NavBar loader styling

--- src/app/api/delete-message/messageid/route.ts
+++ src/app/api/delete-message/messageid/route.ts
@@ -5,12 +5,15 @@ import { authOptions } from "../../auth/[...nextauth]/options";
 import { User } from "next-auth";
 import Router, { useRouter } from "next/router";
 import { useParams } from "next/navigation";
+import { ApiResponse } from "@/types/ApiResponse";
+import { AxiosError } from "axios";
 
 export async function DELETE(req: Request) {
-    await dbConnect();
 
     const params = useParams<{messageId: string}>();
     const messageId = params.messageId
+
+    await dbConnect();
 
     console.log(messageId, 'messageId from delete route');
 
@@ -25,5 +28,31 @@ export async function DELETE(req: Request) {
 
     const user: User = session?.user
 
-    
+    try {
+
+        const updatedResult = await UserModel.updateOne(
+            {id: user._id},
+            {$pull: {messages: {_id: messageId}}}
+        )
+
+        if(updatedResult.modifiedCount == 0) {
+            return Response.json({
+                success: false,
+                message: "Message not found or already deleted, in deleting message route",
+            }, {status: 404})
+        }
+
+        return Response.json({
+            success: true,
+            message: "Message deleted successfully",
+        },{status: 200})
+        
+    } catch (error) {
+        const AxiosError = error as AxiosError<ApiResponse>
+        console.error('Error while deleting message : ', AxiosError?.response?.data?.message);
+        return Response.json({
+            success: false,
+            message: AxiosError?.response?.data?.message,
+        }, {status: AxiosError?.response?.status})
+    }
 }
