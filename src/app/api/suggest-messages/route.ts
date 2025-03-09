2c2f2e644a789b8fa2fd7f700bdeb336b027af92
srikar <srikar.code01@gmail.com>
2025-03-09 10:37:34 +0530

    feat: add ai messages JSON file and integrate suggested messages feature in user profile; enhance message suggestion handling and UI

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -24,18 +24,18 @@ export async function generate(input: string) {
 export async function POST() {
   try {
     const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this:'What's a hobby you've recently started? ||If you could have dinner with any historical figure, who would it be? || What's a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment. ";
-    
+
     const { output } = await generate(prompt);
 
     if (!output || output.trim() === '') {
       throw new Error('Generated text is empty');
     }
 
-    console.log('Generated output:', output);
+    // console.log('Generated output:', output);
 
     return Response.json({
       success: true,
-      messages: output
+      message: output.slice(1, -1)
     });
     
   } catch (error) {
--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -20,59 +20,86 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import axios, { AxiosError } from "axios";
 import { ApiResponse } from "@/types/ApiResponse";
+import { Card, CardContent } from "@/components/ui/card";
+import messages from '@/aimessages.json'
 
 function page() {
   const { status } = useSession();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const params = useParams<{ username: string }>();
   const username = params.username as string;
+  const [suggestedMessages, setSuggestedMessages] = useState<string[]>([]);
+  const [messageLoading, setMessageLoading] = useState(false);
 
   const form = useForm<z.infer<typeof messagesSchema>>({
     resolver: zodResolver(messagesSchema),
     defaultValues: {
-      content: ''
-    }
+      content: "",
+    },
   });
 
   const onSubmit = async (data: z.infer<typeof messagesSchema>) => {
-    setIsSubmitting(true)
+    setIsSubmitting(true);
     try {
-      const AcceptResponse = await axios.get<ApiResponse>('/api/accept-messages')
+      const AcceptResponse = await axios.get<ApiResponse>(
+        "/api/accept-messages"
+      );
 
-      if(!AcceptResponse?.data?.isAcceptingMessage) {
-        toast.error('User is not accepting messages')
-        form.reset()
-        return
+      if (!AcceptResponse?.data?.isAcceptingMessage) {
+        toast.error("User is not accepting messages");
+        form.reset();
+        return;
       }
 
       const response = await axios.post<ApiResponse>("/api/send-message", {
-        username, 
-        content: data.content
+        username,
+        content: data.content,
       });
 
-      if(response?.data?.success) {
-        toast.info(response?.data?.message || 'Successfully sent anonymous message')
-        form.reset()
+      if (response?.data?.success) {
+        toast.info(
+          response?.data?.message || "Successfully sent anonymous message"
+        );
+        form.reset();
       } else {
-        toast.info(response?.data?.message || 'Failed to send message')
+        toast.info(response?.data?.message || "Failed to send message");
       }
     } catch (error) {
-      const axiosError = error as AxiosError<ApiResponse>
-      toast.error(axiosError?.response?.data?.message || 'Error while sending anonymous message')
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast.error(
+        axiosError?.response?.data?.message ||
+          "Error while sending anonymous message"
+      );
     } finally {
-      setIsSubmitting(false)
+      setIsSubmitting(false);
     }
   };
 
   const handleSuggestMessages = async () => {
+    setMessageLoading(true);
     try {
-      console.log('hello world')
+      const messageResponse = await axios.post<ApiResponse>(
+        "/api/suggest-messages"
+      );
+
+      if (messageResponse?.data?.success) {
+        setSuggestedMessages(
+          messageResponse?.data?.message
+            .split("||")
+            .map((message: string) => message.trim())
+        );
+      }
+      setMessageLoading(false);
     } catch (error) {
-      const AxiosError = error as AxiosError<ApiResponse>
-      console.error('Error while getting message suggestions : ', AxiosError);
-      toast.error(AxiosError?.response?.data?.message || 'Error while getting message suggestions')
+      const AxiosError = error as AxiosError<ApiResponse>;
+      console.error("Error while getting message suggestions : ", AxiosError);
+      toast.error(
+        AxiosError?.response?.data?.message ||
+          "Error while getting message suggestions"
+      );
+      setMessageLoading(false);
     }
-  }
+  };
 
   if (status === "loading") {
     return (
@@ -87,13 +114,17 @@ function page() {
       </>
     );
   }
+  console.log(suggestedMessages, "messages from the ai");
+
   return (
     <>
       <main className="w-full h-screen flex flex-col gap-8 container mx-auto items-center mt-10">
         <h1 className="font-bold text-5xl ">Public Profile Link</h1>
 
         <div className="w-full p-8">
-          <h1 className="font-semibold mb-0.5">Send Anonymus Messages to @{username}</h1>
+          <h1 className="font-semibold mb-0.5">
+            Send Anonymus Messages to @{username}
+          </h1>
           <Toaster richColors />
           <Form {...form}>
             <form
@@ -117,24 +148,61 @@ function page() {
                   </FormItem>
                 )}
               />
-            <Button 
-              type="submit" 
-              disabled={!form.watch('content') || isSubmitting}
-            >
-              {isSubmitting ? (
-                <>
-                  <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Loading...
-                </>
-              ) : (
-                "Submit"
-              )}
-            </Button>
+              <Button
+                type="submit"
+                disabled={!form.watch("content") || isSubmitting}
+              >
+                {isSubmitting ? (
+                  <>
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Loading...
+                  </>
+                ) : (
+                  "Submit"
+                )}
+              </Button>
             </form>
           </Form>
         </div>
 
-        <div className="w-full mt-6 p-8 border border-red-500">
-          <Button onClick={() => handleSuggestMessages()}>Suggest Messages</Button>
+        <div className="w-full mt-6 p-8 flex flex-col space-y-6">
+          {messageLoading ? (
+            <Button className="mx-auto w-[35%]" disabled={messageLoading}>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Loading...
+            </Button>
+          ) : (
+            <Button className="mx-auto w-[30%]" onClick={() => handleSuggestMessages()}>
+              Suggest Messages
+            </Button>
+          )}
+
+          {suggestedMessages && suggestedMessages.length > 0 
+            ? suggestedMessages.map((message: string) => (
+                <Card 
+                  className="cursor-pointer"
+                  key={message}
+                  onClick={
+                    () => (form.setValue('content', message, {shouldValidate: true}))
+                  }
+                >
+                  <CardContent>
+                    {message}
+                  </CardContent>
+                </Card>
+              ))
+            : messages.map((message) => (
+                <Card 
+                  className="cursor-pointer"
+                  onClick={
+                    () => (form.setValue('content', message.content, {shouldValidate: true}))
+                  }
+                  key={message.content}
+                >
+                  <CardContent>
+                    {message.content}
+                  </CardContent>
+                </Card>
+              ))
+          }
         </div>
       </main>
     </>
